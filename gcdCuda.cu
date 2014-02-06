b1bb2c29716af426e49b1e7cd56abdf24605b03b
Austin Wylie <awwylie@gmail.com>
2014-02-06 00:03:22 -0800

    gcd can now handle 2D blocks; parallelized isGreaterThanOne

--- gcdCuda.cu
+++ gcdCuda.cu
@@ -1,3 +1,79 @@
+#include "gcdCuda.h"
+#include "io.h"
+
+#define BLOCK_DIM_Y 6 // we'll define this somewhere else eventually
+
+__global__ void cuGCD(u1024bit_t *key, u1024bit_t *key_comparison_list, 
+   uint32_t *bitvector) {
+
+   int keyNum = blockIdx.y * gridDim.x + blockIdx.x;
+   int i = 0;
+   int result = 0;
+   __shared__ u1024bit_t shkey;
+
+   for (i = 0; i < NUM_INTS; i++){
+
+      shkey.number[i] = key.number[i];
+   }
+
+   __syncthreads();
+
+   gcd(shkey->number, key_comparison_list[keyNum]->number);
+
+   if (isGreaterThanOne(key_comparison_list[keyNum]->number)) {
+      (*bitvector) |= (LOW_ONE_MASK << keyNum);
+   }
+}
+
+// result ends up in y; x is also overwritten
+// had to mess around with __syncthreads() in different places;
+// notes from testing are still present
+__device__ void gcd(unsigned int *x, unsigned int *y) {
+   int c = 0;
+
+   while (((x[WORDS_PER_KEY - 1] | y[WORDS_PER_KEY - 1]) & 1) == 0) {
+      __syncthreads(); // definitely needed here
+      shiftR1(x);
+      shiftR1(y);
+      c++;
+   }
+
+   while (isNonZero(x)) {
+      // __syncthreads(); // doesn't seem to be needed
+
+      while ((x[WORDS_PER_KEY - 1] & 1) == 0) {
+         // __syncthreads(); // doesn't seem to be needed
+         shiftR1(x);
+      }
+
+      while ((y[WORDS_PER_KEY - 1] & 1) == 0) {
+         // __syncthreads(); // doesn't seem to be needed
+         shiftR1(y);
+      }
+
+      // __syncthreads(); // doesn't seem to be needed
+      if (geq(x, y)) {
+         // __syncthreads(); // doesn't seem to be needed
+         subtract(x, y);
+
+         // don't have evidence for it,
+         // but I'm pretty sure this is needed
+         __syncthreads();
+         
+         shiftR1(x);
+      }
+      else {
+         // __syncthreads(); // doesn't seem to be needed
+         subtract(y, x);
+         shiftR1(y);
+      }
+   }
+
+   __syncthreads(); // definitely needed here
+
+   shiftL(y, c);
+}
+
 __device__ void shiftR1(unsigned int *arr)
 {
    unsigned int index = threadIdx.x;
@@ -5,15 +81,15 @@ __device__ void shiftR1(unsigned int *arr)
 
    if (index != 0)
    {
-      temp = arr[index + 1];
+      temp = arr[index - 1];
    }
    else
    {
       temp = 0;
    }
 
-   arr[i] >>= 1;
-   arr[i] |= (temp << 31);
+   arr[index] >>= 1;
+   arr[index] |= (temp << WORDS_PER_KEY - 1);
 }
 
 __device__ void shiftL1(unsigned int *arr)
@@ -21,7 +97,7 @@ __device__ void shiftL1(unsigned int *arr)
    unsigned int index = threadIdx.x;
    uint32_t temp;
 
-   if (index != 0)
+   if (index != WORDS_PER_KEY - 1)
    {
       temp = arr[index + 1];
    }
@@ -30,93 +106,105 @@ __device__ void shiftL1(unsigned int *arr)
       temp = 0;
    }
 
-   arr[i] <<= 1;
-   arr[i] |= (temp >> 31);
+   arr[index] <<= 1;
+   arr[index] |= (temp >> WORDS_PER_KEY - 1);
 }
 
-
-/* x must be between 0 and 31  inclusive */
-__device__ void shiftRX(unsigned int *arr, unsigned int x)
-{
-   unsigned int index = threadIdx.x;
-   uint32_t temp;
-
-   if (index != 0)
-   {
-      temp = arr[index + 1];
+__device__ void shiftL(unsigned int *arr, unsigned int x) {
+   int i;
+   for (i = 0; i < x; i++) {
+      shiftL1(arr);
+      __syncthreads(); // OLD
    }
-   else
-   {
-      temp = 0;
-   }
-
-   arr[i] >>= x;
-   arr[i] |= (temp << x);
 }
 
-/* x must be between 0 and 31  inclusive */
-__device__ void shiftLX(unsigned int *arr, unsigned int x)
-{
-   unsigned int index = threadIdx.x;
-   uint32_t temp;
-
-   if (index != 0)
-   {
-      temp = arr[index + 1];
-   }
-   else
-   {
-      temp = 0;
-   }
-
-   arr[i] <<= x;
-   arr[i] |= (temp >> x);
-}
-
-__device__ void subtract(uint32_t *x, uint32_t *y, uint32_t *result)
-{
-   __shared__ uint8_t borrow[WORDS_PER_KEY];
+__device__ void subtract(uint32_t *x, uint32_t *y) {
+   __shared__ uint8_t borrow[BLOCK_DIM_Y][WORDS_PER_KEY];
 
    uint8_t index = threadIdx.x;
 
    // initialize borrow array to 0
-   borrow[index] = 0;
-   __syncthreads();
-
-   result[index] = x[index] - y[index];
+   borrow[threadIdx.y][index] = 0;
+   __syncthreads(); // OLD
 
    if (x[index] < y[index] && index > 0) {
-      borrow[index - 1] = 1;
+      borrow[threadIdx.y][index - 1] = 1;
    }
+
+   x[index] = x[index] - y[index];
 
    int underflow = 0;
 
-   while (__any(borrow[index])) {
-      if (borrow[index]) {
-         underflow = result[index] < 1;
-         result[index] = result[index] - 1;
+   __syncthreads(); // OLD
+
+   while (__any(borrow[threadIdx.y][index])) {
+      if (borrow[threadIdx.y][index]) {
+         underflow = x[index] < 1;
+         x[index] = x[index] - 1;
 
          if (underflow && index > 0) {
-            borrow[index - 1] = 1;
+            borrow[threadIdx.y][index - 1] = 1;
          }
 
-         borrow[index] = 0;
+         borrow[threadIdx.y][index] = 0;
       }
+      __syncthreads(); // OLD
    }
 }
 
 __device__ int geq(uint32_t *x, uint32_t *y) {
-   __shared__ uint8_t pos;
+   __shared__ unsigned int pos[BLOCK_DIM_Y];
+
+   int index = threadIdx.x;
+
+   if (index == 0) {
+      pos[threadIdx.y] = WORDS_PER_KEY - 1;
+   }
+   __syncthreads(); // OLD
+
+   if (x[index] != y[index]) {
+      atomicMin(&pos[threadIdx.y], index);
+   }
+
+   __syncthreads(); // OLD
+   return x[pos[threadIdx.y]] >= y[pos[threadIdx.y]];
+}
+
+__device__ int isNonZero(uint32_t *x) {
+   __shared__ uint8_t nonZeroFound[BLOCK_DIM_Y];
 
    uint8_t index = threadIdx.x;
 
-   pos = WORDS_PER_KEY - 1;
-   __syncthreads();
+   if (index == 0) {
+      nonZeroFound[threadIdx.y] = 0;
+   }
+   __syncthreads(); // OLD
 
-   if (x[index] != y[index]) {
-      atomicMin(&pos, index);
+   if (x[index] != 0) {
+      nonZeroFound[threadIdx.y] = 1;
    }
 
-   return x[pos] >= y[pos];
+   __syncthreads(); // OLD
+   return nonZeroFound[threadIdx.y];
 }
 
+__device__ int isGreaterThanOne(uint32_t *number) {
+   __shared__ uint8_t greaterThanOne[BLOCK_DIM_Y];
+
+   uint8_t index = threadIdx.x;
+
+   if (index == 0) {
+      greaterThanOne[threadIdx.y] = 0;
+   }
+   __syncthreads(); // OLD
+
+   if (index < WORDS_PER_KEY - 1 && number[index] > 0) {
+      greaterThanOne[threadIdx.y] = 1;
+   }
+   else if (index == WORDS_PER_KEY - 1 && number[index] > 1) {
+      greaterThanOne[threadIdx.y] = 1;
+   }
+
+   __syncthreads(); // OLD
+   return greaterThanOne[threadIdx.y];
+}
